cc209d5e6dfd02a2becb1cb3410151af966ef4f4
Víctor Arreaga <arreagav17@gmail.com>
2024-02-03 23:12:24 -0500

    Added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('@pages/auth/auth.routes'),
+  },
+  {
+    path: 'profile',
+    loadComponent: () => import('@pages/auth/profile/profile.page'),
+  },
+];
