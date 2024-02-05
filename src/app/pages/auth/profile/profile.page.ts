f75272bd64cb262fdca5863e5ed750a8bc20a637
Víctor Arreaga <arreagav17@gmail.com>
2024-02-04 20:49:14 -0500

    Added profile page

---
+++ src/app/pages/auth/profile/profile.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gothings-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile.page.html',
+})
+export default class ProfilePage {
+
+}
