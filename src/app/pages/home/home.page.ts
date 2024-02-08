bee3cc918019f4492dca8de3f08a10155c5d5a1a
Víctor Arreaga <arreagav17@gmail.com>
2024-02-07 23:18:58 -0500

    Create home page

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gothings-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.page.html'
+})
+export class HomePage {
+
+}
