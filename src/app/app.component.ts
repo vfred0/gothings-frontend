87863a099cb9cae222033e2bfcec07edc0fb9886
Víctor Arreaga <arreagav17@gmail.com>
2024-02-11 22:28:20 -0500

    Refactored: added header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '@shared/components/header/header.component';
 
 @Component({
   selector: 'gothings-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: ` <router-outlet /> `,
+  imports: [RouterOutlet, HeaderComponent],
+  template: `
+    <gothings-header />
+    <router-outlet />
+  `,
   styles: [],
 })
 export class AppComponent {}
