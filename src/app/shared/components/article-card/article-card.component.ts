e6a3dc2c30c1cd12d0269e11b17f49479f4ab631
Víctor Arreaga <arreagav17@gmail.com>
2024-02-11 22:15:41 -0500

    Implement article card

---
+++ src/app/shared/components/article-card/article-card.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+
+import { Icon } from '@core/enums/icon';
+import { Router } from '@angular/router';
+import { ButtonComponent } from '@shared/components/button/button.component';
+import { ButtonType } from '@core/enums/button-type';
+import { ParseDate } from '@core/models/parse-date';
+import { AppRoute } from '@core/enums/app-route';
+import { ArticleCard } from '@core/models/article-card';
+
+@Component({
+  selector: 'gothings-article-card',
+  standalone: true,
+  imports: [CommonModule, ButtonComponent, NgOptimizedImage, ButtonComponent],
+  templateUrl: './article-card.component.html',
+})
+export class ArticleCardComponent {
+  @Input() articleCard: ArticleCard;
+  protected readonly Icon = Icon;
+  protected readonly ButtonType = ButtonType;
+
+  constructor(protected router: Router) {
+    this.articleCard = {} as ArticleCard;
+  }
+
+  get date(): string {
+    return ParseDate.toRelativeTime(this.articleCard.date);
+  }
+
+  navigateToViewArticle() {
+    this.router.navigate([`${AppRoute.Article}/${this.articleCard.id}`]).then();
+  }
+}
