728d29e2ae7db3cc97cab7ca3d6c1896976c75fc
Víctor Arreaga <arreagav17@gmail.com>
2024-02-11 22:27:03 -0500

    Refactored: set optional

--- src/app/core/dtos/article/article-request.dto.ts
+++ src/app/core/dtos/article/article-request.dto.ts
@@ -1,11 +1,12 @@
 import { Category } from '@core/types/category.type';
 import { State } from '@core/types/state.type';
-
+import { Gender } from '@core/types/gender.type';
 
 export interface ArticleRequestDto {
   title: string;
   description: string;
   category: Category;
   state: State;
+  gender?: Gender;
   images: string[];
 }
