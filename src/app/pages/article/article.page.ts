df567d33f85c7360f73918a37888dc50f0b44aa3
Víctor Arreaga <arreagav17@gmail.com>
2024-02-12 14:47:45 -0500

    Refactored: removed get layout and implements page for article

---
+++ src/app/pages/article/article.page.ts
@@ -0,0 +1,69 @@
+import { Component, inject } from '@angular/core';
+import { HeaderComponent } from '@shared/components/header/header.component';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ArticleCard } from '@core/models/article-card';
+import { AuthService } from '@shared/services/auth.service';
+import { ButtonComponent } from '@shared/components/button/button.component';
+import { HeaderDetailComponent } from '@shared/components/header-detail/header-detail.component';
+import { GalleryComponent } from '@shared/components/gallery/gallery.component';
+import { Icon } from '@core/enums/icon';
+import { ButtonType } from '@core/enums/button-type';
+import { HeaderDetail } from '@core/models/header-detail';
+import { ParseDate } from '@core/utils/parse-date';
+import { AppRoute } from '@core/enums/app-route';
+import { ArticleService } from '@shared/services/article.service';
+
+@Component({
+  selector: 'gothings-article',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    ButtonComponent,
+    HeaderDetailComponent,
+    GalleryComponent,
+  ],
+  templateUrl: './article.page.html',
+})
+export default class ArticlePage {
+  private readonly route: ActivatedRoute;
+  private readonly router: Router;
+  private readonly authService: AuthService;
+  protected readonly Icon = Icon;
+  protected readonly ButtonType = ButtonType;
+  readonly articleCard: ArticleCard;
+  headerDetail: HeaderDetail;
+  private articleService: ArticleService;
+
+  constructor() {
+    this.route = inject(ActivatedRoute);
+    this.router = inject(Router);
+    this.authService = inject(AuthService);
+    this.articleService = inject(ArticleService);
+    this.articleCard = this.router.getCurrentNavigation()!.extras.state![
+      'article'
+    ] as ArticleCard;
+    this.headerDetail = {} as HeaderDetail;
+    this.headerDetail.title = this.articleCard.user.names;
+    this.headerDetail.description = this.articleCard.user.about;
+    this.headerDetail.photo = this.authService.getUserPhoto();
+  }
+
+  onContactWhatsApp() {}
+
+  isCurrentUserArticle() {
+    return this.authService.isCurrentUser(this.articleCard.user.id);
+  }
+
+  onEditArticle() {
+    this.router.navigate([AppRoute.EditArticle, this.articleCard.id]).then();
+  }
+
+  onDeleteArticle() {
+    this.articleService.deleteById(this.articleCard.id);
+    this.router.navigate([AppRoute.Home]).then();
+  }
+
+  get date(): string {
+    return ParseDate.toRelativeTime(this.articleCard.date);
+  }
+}
