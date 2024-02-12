72fb428b2d7b50d7dc21b6d9e8c538fbd381c1f2
Víctor Arreaga <arreagav17@gmail.com>
2024-02-11 22:27:38 -0500

    Implements article card

---
+++ src/app/core/models/article-card.ts
@@ -0,0 +1,34 @@
+import { Category } from '@core/enums/category';
+import { Gender } from '@core/enums/gender';
+import { State } from '@core/enums/state';
+import { ArticleResponseDto } from '@core/dtos/article/article-response.dto';
+import { getValue } from '@core/utils/enum.util';
+
+export class ArticleCard {
+  id: string;
+  title: string;
+  description: string;
+  date: string;
+  category: Category;
+  gender: Gender;
+  state: State;
+  likes: number;
+  dislikes: number;
+  image: string;
+
+  constructor(article: ArticleResponseDto) {
+    this.id = article.id;
+    this.title = article.title;
+    this.description = article.description;
+    this.date = article.date;
+    this.category = getValue(
+      Category,
+      article.category as Category
+    ) as Category;
+    this.gender = getValue(Gender, article.gender as Gender) as Gender;
+    this.state = getValue(State, article.state as State) as State;
+    this.likes = article.likes;
+    this.dislikes = article.dislikes;
+    this.image = article.images[0];
+  }
+}
