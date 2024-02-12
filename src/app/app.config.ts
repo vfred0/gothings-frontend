199d5c16c36ef7c853341eddf9363815d9dc7470
Víctor Arreaga <arreagav17@gmail.com>
2024-02-11 19:49:54 -0500

    Implement gallery for article form

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { HttpClientModule } from '@angular/common/http';
+import { register } from 'swiper/element/bundle';
 
+register();
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
