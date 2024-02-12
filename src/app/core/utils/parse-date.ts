e7da252f65f0452d5005592e49effddcc9bd792e
Víctor Arreaga <arreagav17@gmail.com>
2024-02-11 22:17:53 -0500

    Added utils, for upload image and format date

---
+++ src/app/core/utils/parse-date.ts
@@ -0,0 +1,11 @@
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/es';
+
+export class ParseDate {
+  static toRelativeTime(date: string): string {
+    dayjs.extend(relativeTime);
+    dayjs.locale('es');
+    return dayjs(date).fromNow();
+  }
+}
