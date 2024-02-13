41e51a9166bd16f3c360c086ddd09c6a1d852392
Víctor Arreaga <arreagav17@gmail.com>
2024-02-12 23:00:33 -0500

    Implements user service

---
+++ src/app/shared/services/user.service.ts
@@ -0,0 +1,29 @@
+import { inject, Injectable } from '@angular/core';
+import { environment } from '@env/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '@shared/services/auth.service';
+import { ArticleResponseDto } from '@core/dtos/article/article-response.dto';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private readonly API_URL: string = environment.apiBaseUrl + '/users';
+  private http: HttpClient = inject(HttpClient);
+  private readonly authService: AuthService = inject(AuthService);
+
+  private getToken() {
+    return {
+      headers: {
+        Authorization: `Bearer ${this.authService.getToken()}`,
+      },
+    };
+  }
+
+  getArticles() {
+    return this.http.get<Array<ArticleResponseDto>>(
+      `${this.API_URL}/${this.authService.getUserId()}/articles`,
+      this.getToken()
+    );
+  }
+}
