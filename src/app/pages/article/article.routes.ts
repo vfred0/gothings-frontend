d56f1599c6fc96f0e14ec7e27ccb43a48d542619
Víctor Arreaga <arreagav17@gmail.com>
2024-02-13 13:11:57 -0500

    Refactored: added article routes

--- src/app/pages/article/article.routes.ts
+++ src/app/pages/article/article.routes.ts
@@ -1,47 +1,34 @@
 import { Routes } from '@angular/router';
 import { AppRoute } from '@core/enums/app-route';
-import { userIsAuthenticated } from '@shared/guards/auth.guard';
 
 export const routes: Routes = [
   {
-    path: AppRoute.Auth,
-    loadChildren: () => import('@pages/auth/auth.routes'),
-  },
-  {
-    path: AppRoute.Home,
-    loadChildren: () =>
-      import('@pages/article/article.routes').then(m => m.routes),
-    canActivate: [userIsAuthenticated],
+    path: '',
+    loadComponent: () =>
+      import('@pages/article/explore-articles/explore-articles.page').then(
+        m => m.ExploreArticlesPage
+      ),
   },
   {
     path: AppRoute.Article,
     loadComponent: () => import('@pages/article/article.page'),
-    canActivate: [userIsAuthenticated],
   },
   {
     path: `${AppRoute.EditArticle}/:id`,
     loadComponent: () =>
       import('@pages/article/update-article/update-article.form'),
-    canActivate: [userIsAuthenticated],
   },
   {
     path: AppRoute.PublishArticle,
     loadComponent: () => import('@pages/article/new-article/new-article.form'),
-    canActivate: [userIsAuthenticated],
   },
   {
     path: `${AppRoute.Article}/:id`,
     loadComponent: () => import('@pages/article/article.page'),
-    canActivate: [userIsAuthenticated],
   },
 
   {
     path: `${AppRoute.MyArticles}`,
     loadComponent: () => import('@pages/article/my-articles/my-articles.page'),
-    canActivate: [userIsAuthenticated],
-  },
-  {
-    path: '**',
-    redirectTo: '',
   },
 ];
